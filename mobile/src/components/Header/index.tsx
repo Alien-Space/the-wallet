4aeb57bf583434f11352f7ed8e8c2db93287b66c
AlienDev66 <p.capitango65@gmail.com>
2020-10-21 06:27:48 +0100

    :sparkles: Initial Screen.

---
+++ mobile/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import logoImage from "../../assets/logotipo/theWallet.png";
+
+import { Container, Image, TextHeader } from "./styles";
+
+interface PropsHeader {
+  title: string;
+}
+
+function Header({ title }: PropsHeader) {
+  return (
+    <Container>
+      <Image source={logoImage} />
+      <TextHeader>{title}</TextHeader>
+    </Container>
+  );
+}
+
+export default Header;
