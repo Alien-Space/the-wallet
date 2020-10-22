0edd8cb4e856aa8559e06ac2f3e51f6d68d1c3fd
jmamadeu <jmamadeu2000@gmail.com>
2020-10-22 21:02:32 +0100

    :zap: chore: Database configuration

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,8 +1,8 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
-import { createConnection } from "typeorm";
+import { createConnection } from 'typeorm';
 
 export const createSqliteConnection = () =>
   createConnection()
-    .then((res) => console.log("Database is UP", res.name))
-    .catch((err) => console.log("Database is Down", err.message));
+    .then((res) => console.log('Database is UP', res.name))
+    .catch((err) => console.log('Database is Down', err.message));
