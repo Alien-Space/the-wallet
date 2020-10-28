4b6597987d55d9a2724e190d4413dca194f2fe36
jmamadeu <jmamadeu2000@gmail.com>
2020-10-29 00:43:08 +0100

    :zap: Add new route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import UserController from './controllers/UserController';
+import { SessionController, UserController } from './controllers';
 
 const routes = Router();
 
@@ -7,6 +7,14 @@ routes.post('/users', async (request, response) => {
   const userController = new UserController();
 
   const { content, statusCode } = await userController.create(request.body);
+
+  return response.status(statusCode).json(content);
+});
+
+routes.post('/sessions', async (request, response) => {
+  const sessionController = new SessionController();
+
+  const { content, statusCode } = await sessionController.create(request.body);
 
   return response.status(statusCode).json(content);
 });
