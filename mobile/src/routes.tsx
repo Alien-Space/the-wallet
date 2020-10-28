b665eaf12bbecef96dd894c479a742d7e4a0cf74
AlienDev66 <p.capitango65@gmail.com>
2020-10-29 00:59:28 +0100

    :sparkles: Terminado o Dashboard e a teça de cadastro de Documento.

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Dashboard from "./pages/Dashboard";
+import CreateDocument from "./pages/CreateDocument";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -11,11 +12,13 @@ export default function Routes() {
   return (
     <NavigationContainer>
       <Navigator
+        initialRouteName="Dashboard"
         screenOptions={{
           headerShown: false,
           cardStyle: { backgroundColor: "transparent" },
         }}>
         <Screen name="Dashboard" component={Dashboard} />
+        <Screen name="CreateDocument" component={CreateDocument} />
       </Navigator>
     </NavigationContainer>
   );
