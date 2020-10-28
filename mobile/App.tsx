7de77080ed813f3d212f66f11a8cffcac1806619
AlienDev66 <p.capitango65@gmail.com>
2020-10-29 00:59:19 +0100

    :sparkles: Terminado o Dashboard e a teça de cadastro de Documento.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Header from "./src/components/Header";
+import { StyleSheet, View } from "react-native";
 import {
   useFonts,
   RalewayDots_400Regular,
 } from "@expo-google-fonts/raleway-dots";
+import { Questrial_400Regular } from "@expo-google-fonts/questrial";
 import Routes from "./src/routes";
 
 export default function App() {
   const [fontsLOaded] = useFonts({
     RalewayDots_400Regular,
+    Questrial_400Regular,
   });
   if (!fontsLOaded) {
     return null;
@@ -27,8 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#0D1F3C",
-    padding: 24,
-    // alignItems: "center",
-    // justifyContent: "center",
+    paddingTop: 30,
+    padding: 15,
   },
 });
