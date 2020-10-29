c9bd846aa6f434cf64a4713e2a6c4eb6737b7f19
AlienDev66 <p.capitango65@gmail.com>
2020-10-29 01:02:32 +0100

    Merge branch 'main' of https://github.com/OriuXus/the-wallet-frontend into main

---
+++ server/src/controllers/UserController.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+import { User } from '../database/entities';
+
+interface createUser {
+  name: string;
+  email: string;
+  contact: string;
+  sex: string;
+  address: string;
+  password: string;
+}
+
+export default class UserController {
+  async create({ email, ...rest }: createUser) {
+    try {
+      const userRepository = getRepository(User);
+
+      const userExists = await userRepository.findOne({ where: { email } });
+
+      if (userExists) {
+        throw new Error('User already exists');
+      }
+
+      const newUser = userRepository.create({ email, ...rest });
+
+      await userRepository.save(newUser);
+
+      return { statusCode: 201, content: { data: newUser } };
+    } catch (err) {
+      return { statusCode: 400, content: { message: err.message } };
+    }
+  }
+}
