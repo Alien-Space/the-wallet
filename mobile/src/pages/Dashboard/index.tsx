88ac736d42d5569cfce9c90170d33852a6725fe0
AlienDev66 <p.capitango65@gmail.com>
2020-10-29 01:00:02 +0100

    :sparkles: Terminado o Dashboard e a teça de cadastro de Documento.

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,14 +1,58 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 import Header from "../../components/Header";
 
-import { Container } from "./styles";
+import {
+  Container,
+  MenuOption,
+  MenuOptionContaierOne,
+  MenuOptionContaierTwo,
+  Footer,
+} from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 const Dashboard: React.FC = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToCreateDocument() {
+    navigation.navigate("CreateDocument");
+  }
+
   return (
     <Container>
-      <Header title="About" />
-      <Text style={styles.textDocument}>Seus {"\n"} documento</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header title="About" />
+        <Text style={styles.textDocument}>Seus {"\n"}documento</Text>
+
+        <MenuOptionContaierOne>
+          <MenuOption style={{ backgroundColor: "#fdb32a" }}>
+            <Feather name="plus" size={20} color="#FFF" />
+          </MenuOption>
+          <MenuOption>
+            <Feather name="plus" size={20} color="#FFF" />
+          </MenuOption>
+        </MenuOptionContaierOne>
+
+        <MenuOptionContaierTwo>
+          <MenuOption>
+            <Feather name="plus" size={20} color="#FFF" />
+          </MenuOption>
+          <MenuOption>
+            <Feather name="plus" size={20} color="#FFF" />
+          </MenuOption>
+        </MenuOptionContaierTwo>
+
+        <Footer>
+          <Text style={styles.textDocument}>Adicione novo {"\n"}documento</Text>
+          <MenuOption
+            style={{ backgroundColor: "#fdb32a", width: 66, height: 66 }}
+            onPress={handleNavigateToCreateDocument}>
+            <Feather name="plus" size={20} color="#FFF" />
+          </MenuOption>
+        </Footer>
+      </ScrollView>
     </Container>
   );
 };
