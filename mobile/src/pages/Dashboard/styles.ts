2953c60bc8df459633f14e3e94e25ab552ee3e67
AlienDev66 <p.capitango65@gmail.com>
2020-10-29 01:00:04 +0100

    :sparkles: Terminado o Dashboard e a teça de cadastro de Documento.

--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,5 +1,31 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { RectButton } from "react-native-gesture-handler";
 
-export const Container = styled.View`
+export const Container = styled.View``;
 
+export const MenuOption = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+  background-color: #dbdbdb;
+  width: 160px;
+  height: 170px;
+  border-radius: 20px;
+`;
+
+export const MenuOptionContaierOne = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const MenuOptionContaierTwo = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Footer = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  justify-content: space-between;
 `;
