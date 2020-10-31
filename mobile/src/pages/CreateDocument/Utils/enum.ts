d6aa4a5b86e3e71c5c4a09d28532024b3429307d
AlienDev66 <p.capitango65@gmail.com>
2020-10-31 16:48:33 +0100

    :sparkles: Dando inicio ao cadastro de usuário.

---
+++ mobile/src/pages/CreateDocument/Utils/enum.ts
@@ -0,0 +1,8 @@
+export const stringImports = {
+  GESTUREHANDLER: "react-native-gesture-handler",
+  LOGOREGISTER: "../../assets/logotipo/logoRegister.png",
+  GOBACK: "../../assets/Icones/goBack.png",
+  CHECK: "../../assets/Icones/icon/check.png",
+  USENAVIGATION: "@react-navigation/native",
+  REACTNATIVE: "react-native",
+};
