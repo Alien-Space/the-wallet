3ca9f950d93b51abc97a3fc63c6c2b099472b166
AlienDev66 <p.capitango65@gmail.com>
2020-10-31 16:48:36 +0100

    :sparkles: Dando inicio ao cadastro de usuário.

---
+++ mobile/src/pages/UserRegister/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+const UserRegister: React.FC = () => {
+  return (
+    <Container>
+      <Text>UserRegister</Text>
+    </Container>
+  );
+};
+
+export default UserRegister;
