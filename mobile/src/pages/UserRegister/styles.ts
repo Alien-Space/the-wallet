15fa51fc92d00c6e86a9216b8221132db6531a47
AlienDev66 <p.capitango65@gmail.com>
2020-10-31 20:43:27 +0100

    :sparkles: Terminando layut das páginas.

--- mobile/src/pages/UserRegister/styles.ts
+++ mobile/src/pages/UserRegister/styles.ts
@@ -1,5 +1,38 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
-
+  flex: 1;
+  background-color: #0d1f3c;
+`;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 98%;
+`;
+export const Image = styled.Image``;
+export const ImageBack = styled.Image``;
+export const TextNewDocument = styled.Text`
+  margin-top: 20%;
+  color: #fff;
+  font-size: 25px;
+  font-family: Questrial_400Regular;
+`;
+export const Form = styled.View`
+  margin-top: 5%;
+  border-radius: 20px;
+  background-color: #fbfbfb;
+  height: 290px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+export const InputContainer = styled.View`
+  margin-top: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const ContainerOne = styled.View``;
+export const ContainerTwo = styled.View``;
+export const ImageCheck = styled.Image``;
+export const ButtonSubmitContainer = styled.View`
+  padding-top: 20px;
 `;
