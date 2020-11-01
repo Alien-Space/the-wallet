ae2fdb878089c1cf5bcc7e6b3245adce8925e6f4
jmamadeu <jmamadeu2000@gmail.com>
2020-11-01 21:55:00 +0100

    :zap: chore: Add default exports

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,2 +1,3 @@
 export { default as UserController } from './UserController';
 export { default as SessionController } from './SessionController';
+export { default as DocumentController } from './DocumentController';
