4f6f4b2f2b8f602d2970d17962f0ad94ae395d62
AlienDev66 <p.capitango65@gmail.com>
2020-12-11 23:50:59 +0100

    :tada: More one.

--- mobile/src/pages/Logon/index.tsx
+++ mobile/src/pages/Logon/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { useLayoutEffect, useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import Header from "../../components/Header";
 import imgQR from "../../assets/Icones/icon/icon/qrcode.png";
 
@@ -17,42 +17,105 @@ import { useNavigation } from "@react-navigation/native";
 
 const Logon: React.FC = () => {
   const navigation = useNavigation();
-  const [focusedInputTwo, setFocusedInputTwo] = useState(false);
+  const inputOne = useRef<TextInput | null>(null);
+  const inputTwo = useRef<TextInput | null>(null);
+  const inputThree = useRef<TextInput | null>(null);
+  const inputInitial = useRef<TextInput | null>(null);
+  const [valueLogon, setValueLogon] = useState("");
 
+  function onInputOneChange() {
+    inputOne?.current?.focus();
+  }
+  function onInputTwoChange() {
+    inputTwo?.current?.focus();
+  }
+  function onInputThreeChange() {
+    inputThree?.current?.focus();
+  }
+  function onInputInitialChange() {
+    inputInitial?.current?.focus();
+  }
   function handleGoToRegister() {
     navigation.navigate("UserRegister");
   }
-  function onInputChangeFocusInOtherInput() {
-    setFocusedInputTwo(true);
+  function handleGoToDashboard() {
+    navigation.navigate("Dashboard");
+  }
+
+  function getTextOnInput(text: string) {
+    setValueLogon(valueLogon + text);
+    console.log(valueLogon);
   }
 
   return (
     <Container>
-      <ContainerHeader>
-        <Header />
-        <ImageQR source={imgQR} />
-      </ContainerHeader>
-      <Text style={styles.textDocument}>
-        Seus documentos {"\n"}mais próximos de si.
-      </Text>
-      <InputPinContainer>
-        <PinInputs>
-          <InputPin
-            autoFocus={true}
-            maxLength={1}
-            onChangeText={onInputChangeFocusInOtherInput}
-          />
-          <InputPin maxLength={1} autoFocus={focusedInputTwo} />
-          <InputPin maxLength={1} />
-          <InputPin maxLength={1} />
-        </PinInputs>
-      </InputPinContainer>
+      <View>
+        <ContainerHeader>
+          <Header />
+          <RectButton onPress={handleGoToDashboard}>
+            <ImageQR source={imgQR} />
+          </RectButton>
+        </ContainerHeader>
+        <Text style={styles.textDocument}>
+          Seus documentos {"\n"}mais próximos de si.
+        </Text>
+        <InputPinContainer>
+          <PinInputs>
+            <InputPin
+              ref={inputInitial}
+              selectionColor={"#000000"}
+              onChangeText={(text) => {
+                if (!text) return;
+                setValueLogon(text);
+                onInputOneChange();
+              }}
+              maxLength={1}
+            />
+            <InputPin
+              ref={inputOne}
+              onChangeText={(text) => {
+                if (!text) return;
+                getTextOnInput(text);
+                onInputTwoChange();
+              }}
+              onKeyPress={({ nativeEvent }) => {
+                nativeEvent.key === "Backspace" ? onInputInitialChange() : null;
+              }}
+              maxLength={1}
+            />
+            <InputPin
+              ref={inputTwo}
+              maxLength={1}
+              onChangeText={(text) => {
+                if (!text) return;
 
-      <RectButtonContainer>
-        <RectButton onPress={handleGoToRegister} style={styles.CreateAccount}>
-          <Text style={styles.textRegister}>Registar-se</Text>
-        </RectButton>
-      </RectButtonContainer>
+                getTextOnInput(text);
+                onInputThreeChange();
+              }}
+              onKeyPress={({ nativeEvent }) => {
+                nativeEvent.key === "Backspace" ? onInputOneChange() : null;
+              }}
+            />
+            <InputPin
+              ref={inputThree}
+              maxLength={1}
+              onChangeText={(text) => {
+                if (!text) return;
+                getTextOnInput(text);
+                console.log(valueLogon);
+              }}
+              onKeyPress={({ nativeEvent }) => {
+                nativeEvent.key === "Backspace" ? onInputTwoChange() : null;
+              }}
+            />
+          </PinInputs>
+        </InputPinContainer>
+        <RectButtonContainer>
+          <RectButton onPress={handleGoToRegister} style={styles.CreateAccount}>
+            <Text style={styles.textRegister}>Registar-se</Text>
+          </RectButton>
+        </RectButtonContainer>
+      </View>
     </Container>
   );
 };
